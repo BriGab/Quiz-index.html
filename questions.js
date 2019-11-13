2f710254a7bc195ccfb89d6ddce6f414af2d7b1d
Bri Gab <brigabiola@outlook.com>
2019-11-13 14:53:44 -0700

    questions added

--- questions.js
+++ questions.js
@@ -1,13 +1,28 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "How would you select this specific element <h1 class=\"header\"> using DOM?",
+      choices: ["document.getElementbyId()", "document.getElementsbyClassName()", "$(<h1>)", "document.querySelector(#header)"],
+      answer: "document.getElementsbyClassName()"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "A function can be intiated by calling the name of the function followed by ____",
+      choices: ["parantheses & semi-colon", "curly brackets & semi-colon", "parantheses", "semi-colon"],
+      answer: "parentheses & semi-colon"
     },
-    ///etc.
+    {
+      title: "Where do you insert your script tag with Javascript inside your HTML page?",
+      choices: ["In the <head> element", "right below the beginning <body> tag", "Right above the ending </body> tag", "In a <div> tag"],
+      answer: "Right above the ending </body> tag"
+    },
+    {
+      title: "How do you set a variable in Javascript?",
+      choices: ["Write the name and set it equal to whatever you'd like", "write var ____ and then the variable name", "write function equals whatever you'd like", "variable cannot be created"],
+      answer: "write var ____ and then the variable name"
+    },
+    {
+      title: "How would you alert Hello World, to a user?" ,
+      choices: ["function(Hello World)", "if (Hello World)", "alert = Hello World", "alert(\"Hello World\")"],
+      answer: "alert(\"Hello World\")"
+    }
   ];
+  
--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 var startButton = document.querySelector("#startButton");
-
-var totalSeconds = 75;
-var secondsElapsed = 0
-var interval;
+var body = document.getElementById("body");
 //create a countdown for the quiz
-
+//create a button that initiates the quiz
 
 var timeleft = 75;
-var downloadTimer = setInterval(function(){
-  document.getElementById("counter").innerHTML = timeleft;
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished"
-  }
-}, 1000);
-
-
-//create a button that initiates the quiz
+startButton.addEventListener("click", function(){
+  var downloadTimer = setInterval(function(){
+    body.textContent = "";
+    document.getElementById("counter").innerHTML = timeleft;
+    timeleft -= 1;
+    if(timeleft <= 0){
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").innerHTML = "Finished"
+    }
+  }, 1000);
 //show user the first question
+for (var i = 0; i < questions.length; i++) {
+
+  var title = questions[i].title;
+  var choices = questions[i].choices;
+  var answer = questions[i].answer;
+  
+  var tr = document.createElement("tr");
+  var td = document.createElement("td");
+
+  td.innerHTML = title;
+
+
+}
+
+// var tr = document.createElement("tr");
+//         // create a table data cell (td) and inside build out an a tag w/ the link and name of link
+//         var td = document.createElement("td");
+//         // <a href ='linkehere' target='_blank'>nameHere</a>
+//         td.innerHTML = "<a href='" + url + "' target='_blank'>" + name + "</a>";
+//         // td.innerHTML = `<a href=${url} target="_blank">${name}</a>`; //does the same thing as teh line above
+//         // append the td's into the tr 
+//         tr.appendChild(td);
+//         // Since tr is living only in javascript we have to append our tr to the table
+//         bookmarksTable.appendChild(tr);
+//         // If we add the delete button, wire up the delete click event
+
+
+
+});
+
+
+
 //Once user selects answer show whether they are correct or incorrect 
 //Keep track of this score 
 //Move on to the next question
