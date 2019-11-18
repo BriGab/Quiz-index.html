b0c8d0656e21d528465a482e19d17ae563d7bd48
Bri Gab <brigabiola@outlook.com>
2019-11-18 14:50:27 -0700

    answers added w/ correct/incorrect

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 var questions = [
     {
       title: "How would you select this specific element <h1 class=\"header\"> using DOM?",
-      choices: ["1. document.getElementbyId()", "2. document.getElementsbyClassName()", "3. $(<h1>)", "4. document.querySelector(#header)"],
-      answer: "Answer: document.getElementsbyClassName()"
+      choices: ["A. document.getElementbyId()","B. document.getElementsbyClassName()", "C. $(<h1>)", "E. document.querySelector(#header)"],
+      answer: "B. document.getElementsbyClassName()"
     },
     {
       title: "A function can be intiated by calling the name of the function followed by ____",
-      choices: ["1. parantheses & semi-colon", "2. curly brackets & semi-colon", "3. parantheses", "4. semi-colon"],
-      answer: "Answer: parentheses & semi-colon"
+      choices: ["A. parantheses & semi-colon", "B. curly brackets & semi-colon", "C. parantheses", "D. semi-colon"],
+      answer: "A. parantheses & semi-colon"
     },
     {
       title: "Where do you insert your script tag with Javascript inside your HTML page?",
-      choices: ["1. In the <head> element", "2. right below the beginning <body> tag", "3. Right above the ending </body> tag", "4. In a <div> tag"],
-      answer: "Answer: Right above the ending </body> tag"
+      choices: ["A. in the <head> element", "B. right below the beginning <body> tag", "C. right above the ending </body> tag", "D. in a <div> tag"],
+      answer: "C. right above the ending </body> tag"
     },
     {
       title: "How do you set a variable in Javascript?",
-      choices: ["1. Write the name and set it equal to whatever you'd like", "2. write var ____ and then the variable name", "3. write function equals whatever you'd like", "4. variable cannot be created"],
-      answer: "Answer: write var ____ and then the variable name"
+      choices: ["A. write the name and set it equal to whatever you'd like", "B. write var ____ and then the variable name", "C. write function equals whatever you'd like", "D. variable cannot be created"],
+      answer: "B. write var ____ and then the variable name"
     },
     {
       title: "How would you alert Hello World, to a user?" ,
-      choices: ["1. function(Hello World)", "2. if (Hello World)", "3. alert = Hello World", "4. alert(\"Hello World\")"],
-      answer: "Answer: alert(\"Hello World\")"
+      choices: ["A. function(Hello World)", "B. if (Hello World)", "C. alert = Hello World", "D. alert(\"Hello World\")"],
+      answer: "D. alert(\"Hello World\")"
     }
   ];
   
--- script.js
+++ script.js
@@ -2,69 +2,77 @@ var startButton = document.querySelector("#startButton");
 var question = document.getElementById("question");
 var description = document.getElementById("description");
 var body = document.getElementById("body");
+var header = document.getElementById("#heading");
+// var confirm = document.getElementsByClassName("rightWrong");
+var timeleft = 75;
+var currentIndex = 0;
+var timerId;
+var correct = 0, incorrect = 0;
 //create a countdown for the quiz
 //create a button that initiates the quiz
 
-var timeleft = 75;
-
-
-
-
-function getQuestion () {
-  currentIndex++;
-  var title = questions[currentIndex].title;
-  var choices = questions[currentIndex].choices;
-  var answer = questions[currentIndex].answer;
-
-  var header = document.createElement("h1");
-
-  header.innerHTML = title;
-  body.appendChild(header);
-  console.log (title);
-
-
-for (var i = 0; i < choices.length; i++) {
-
-  
-  
-  var tr = document.createElement("tr");
-  var td = document.createElement("td");
-
-  td.innerHTML = choices[i];
-  tr.appendChild(td);
-  body.appendChild(tr);
-
-  console.log(title);
-  console.log(choices);
-  console.log(answer);
-  
-}
-}
-function timer(){
-  // event.preventDefault();
- 
-  setInterval(function(){
-    console.log("timer")
-    body.textContent = "";
-    document.getElementById("counter").innerHTML = timeleft;
-    timeleft -= 1;
-    if(timeleft === 0){
-      clearInterval(timer);
-      document.getElementById("countdown");
-    }
-  },1000);
-}
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function () {
+  startButton.remove();
+  // description.remove();
   timer();
   getQuestion();
-
-
 });
 
+function timer() {
+  timerId = setInterval(function () {
+    document.getElementById("counter").innerHTML = timeleft;
+    timeleft--;
+    if (timeleft <= 0) {
+      clearInterval(timerId);
+      document.getElementById("counter").innerHTML = "Finished";
+    }
+  }, 1000);
+}
 
-var currentIndex = 0;
 
 //show user the first question
+function getQuestion() {
+
+  var title = questions[currentIndex].title;
+  document.getElementById("heading").innerText = title;
+
+  question.innerHTML = "";
+  var choices = questions[currentIndex].choices;
+  for (var i = 0; i < choices.length; i++) {
+
+    var btn = document.createElement("BUTTON");
+    btn.classList.add("btn")
+    btn.classList.add("btn-info")
+    btn.classList.add("btn-sm")
+    btn.classList.add("btn-group-vertical")
+    btn.classList.add("btnStyle")
+    btn.value = choices[i];
+    btn.innerText = choices[i];
+
+    //  Assign function (bind 'this' to each instance of the function)
+    btn.onclick = clickAnswer;
+
+    question.appendChild(btn);
+  }
+}
+
+function clickAnswer() {
+  //  this === btn
+  if (this.value === questions[currentIndex].answer) {
+    document.getElementById("rightWrong").textContent = "Correct";
+    correct++;
+  } else {
+    document.getElementById("rightWrong").textContent = "Incorrect";
+    incorrect++;
+  }
+  currentIndex++;
+  if (currentIndex < questions.length) {
+    getQuestion();
+  } else {
+    clearInterval(timerId);
+    console.log("GAME OVER!")
+  }
+};
 
 
 
@@ -74,5 +82,20 @@ var currentIndex = 0;
 //Move on to the next question
 //repeat until all questions have been asked and answered 
 //Show user's final score and have them enter initials to save highscore
-//when inititals are submitted change to the high score screen and have an options to clear 
+//when inititals are submitted change to the high score screen and have an options to clear
 
+
+
+// var timeleft = 75;
+// function timer(){
+//   setInterval(function(){
+//     console.log("timer")
+//     body.textContent = "";
+//     document.getElementById("counter").innerHTML = timeleft;
+//     timeleft -= 1;
+//     if(timeleft === 0){
+//       clearInterval(timer);
+//       document.getElementById("countdown");
+//     }
+//   }, 1000);
+// }
